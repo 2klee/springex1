810c3a4ec0ac2d2386343432bcd89acccede2a8f
admin <terious.kr@gmail.com>
2024-01-09 10:40:45 +0900

    스프링 환경설정

---
+++ src/main/java/com/example/springex1/dto/TodoDTO.java
@@ -0,0 +1,18 @@
+package com.example.springex1.dto;
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TodoDTO {
+  private Long tno;
+  private String title;
+  private LocalDate dueDate;
+  private boolean finished;
+  private String writer;
+
+}
